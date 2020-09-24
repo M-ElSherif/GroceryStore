57df7d715ec9b28ee5449d1ca329adbac7c4faff
Mohamed El Sherif <mohamed.elsherif90@gmail.com>
2020-09-24 18:30:52 -0400

    Add project files.

---
+++ GroceryStore/Pages/Details.cshtml.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using GroceryStore.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Server.IIS.Core;
+
+namespace GroceryStore.Pages
+{
+    public class DetailsModel : PageModel
+    {
+        public List<GroceryItem> Foods { get; set; }
+        public GroceryItem CurrentFood { get; set; }
+        [BindProperty]
+        public int Quantity { get; set; }
+        public double TotalCost { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(string name)
+        {
+            using (StreamWriter writer = new StreamWriter("log.txt", append:true))
+            {
+                await writer.WriteLineAsync($"{DateTime.Now} {name}");
+            }
+            this.Foods = Inventory.ToList();
+            this.CurrentFood = this.Foods.
+                Find(food => food.Name.ToLower().Equals(name.ToLower()));
+            if (this.CurrentFood == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public double CalcTotalCost()
+        {
+            return this.Quantity * this.CurrentFood.Price;
+        }
+    }
+}
---
+++ GroceryStore/Pages/Index.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+using GroceryStore.Models;
+
+namespace GroceryStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+
+        [BindProperty]
+        public int Rating { get; set; }
+        [BindProperty]
+        public string Feedback { get; set; }
+        [BindProperty]
+        public List<GroceryItem> Foods { get; set; }
+        [BindProperty]
+        public string FirstName { get; set; }
+        [BindProperty]
+        public string LastName { get; set; }
+
+        public void OnGet()
+        {
+            this.Foods = Inventory.ToList();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            using (StreamWriter writer = new StreamWriter("suggestions.txt", append: true))
+            {
+                await writer.WriteLineAsync(this.FirstName + " " + this.LastName);
+            }
+            return RedirectToPage("./Index");
+        }
+
+    }
+}
